cd16e62dac38ed780d2eca6b1b1850b3b7400fe6
ainex <ainex.niene@gmail.com>
2013-08-29 21:10:37 +0700

    first commit

---
+++ AlgorithmImpl/src/main/java/com/ulyanona/unionfind/UnionFind.java
@@ -0,0 +1,17 @@
+package com.ulyanona.unionfind;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ainex
+ * Date: 29.08.13
+ * Time: 20:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class UnionFind {
+    public static void main( String[] args )
+    {
+       QuickFindUF union= new QuickFindUF(10);
+       union.printUnion();
+
+    }
+}
